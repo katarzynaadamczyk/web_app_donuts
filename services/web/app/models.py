c6574784b2ba15e2a77e8c698797d643cd7a6ec9
Katarzyna <kasia.cholost@gmail.com>
2025-01-27 12:49:53 +0100

    updated models

--- services/web/app/models.py
+++ services/web/app/models.py
@@ -3,12 +3,27 @@ create table in db
 '''
 from app import db
 
-class User(db.Model):
-    __tablename__ = "users"
+class Donuts(db.Model):
+    __tablename__ = "donuts"
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+    kcal = db.Column(db.Float(5), nullable=False)
+    weight = db.Column(db.Float(5), nullable=False)
+    manufacturer_id = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, email):
-        self.email = email
+    def __init__(self, name, kcal, weight, manufacturer_id):
+        self.name = name
+        self.kcal = kcal
+        self.weight = weight
+        self.manufacturer_id = manufacturer_id
+
+
+class Manufacturers(db.Model):
+    __tablename__ = "manufacturers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+
+    def __init__(self, name):
+        self.name = name
