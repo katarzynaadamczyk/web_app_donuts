aeb314810acee1723e4c756b2edc3a9d159a655c
Katarzyna <kasia.cholost@gmail.com>
2025-01-30 12:49:02 +0100

    prepared info about algorithms

---
+++ services/web/app/algorithms.py
@@ -0,0 +1,21 @@
+'''
+algorithms for finding donuts 
+to fill a person for Fat Thursday
+
+'''
+
+
+def pick_me_donuts_1(donuts: dict, belly_max_size: int) -> dict:
+    '''
+    Finds donuts to fill a person's belly when a person can buy 
+    unlimited number of donuts in donuts dict
+
+    Args:
+        donuts: dict (str: dict (str: int/float))
+        belly_max_size: max weight of donuts a person can eat
+    
+    Returns:
+        dict (str: int) - name: number
+    '''
+    
+    pass
