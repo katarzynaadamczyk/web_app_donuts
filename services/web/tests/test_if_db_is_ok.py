7e87725c7db433f73d153d15b44c1f5991d3e268
Katarzyna <kasia.cholost@gmail.com>
2025-02-05 22:36:05 +0100

    added testing available donuts - to be finished

--- services/web/tests/test_if_db_is_ok.py
+++ services/web/tests/test_if_db_is_ok.py
@@ -1,14 +1,27 @@
-from app.utils import get_all_available_manufacturers
+'''
+test if correct data is in database
+if so scraper works correctly
+'''
+from app.utils import get_all_available_donuts, get_all_available_manufacturers
 import pytest
 
 
 def test_get_all_available_manufacturers_returns_3_manufacturers(app_with_db):
     '''
-    testing if get_correct_ip_address returns a valid ip_address
+    testing if get_all_available_manufacturers returns 3 manufacturers as in database.json
     '''
     with app_with_db.app_context():
         manufacturers = get_all_available_manufacturers()
         assert len(manufacturers) == 3
 
-    # TODO
-    # test for more different ip_addresses
+        assert set(['Putka', 'Oskroba', 'Grzybki']) == set([item[1] for item in manufacturers])
+
+
+def test_get_all_available_donuts(app_with_db):
+    '''
+    testing if get_all_available_manufacturers returns 3 manufacturers as in database.json
+    '''
+    with app_with_db.app_context():
+        donuts = get_all_available_donuts()
+        assert len(donuts) == 16
+
