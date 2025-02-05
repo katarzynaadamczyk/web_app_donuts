2ef38b7ed2bab43a4c706b345dc2104104c2b514
Katarzyna <kasia.cholost@gmail.com>
2025-02-05 12:21:38 +0100

    added descriptions, eliminated duplicates etc from file utils_scraper.py

--- services/web/app/utils_scraper.py
+++ services/web/app/utils_scraper.py
@@ -10,7 +10,12 @@ from sqlalchemy import select, and_
 import re
 from app.models import *
 
+
 class Scraper:
+    '''
+    class Scraper builds an object to get all needed data
+    for project's database
+    '''
     # define json dict keys in case they change over time
     LINK = 'html'
     NAME = 'name'
@@ -30,25 +35,74 @@ class Scraper:
         '''
         adds to database all donuts from given webpage to Donuts Table
         and their manufacturer (item[NAME]) to Manufacturers Table
+
+        Args:
+            item: dict(str: str/int) {Scraper.LINK: str (link to a web page),
+                                      Scraper.NAME: str (name of manufacturer),
+                                      Scraper.N: int (Optional, indicates how many subpages to visit)}
         '''
+        # add manufacturer to db and get its id
         manufacturer_id = self.add_manufacturer(item[Scraper.NAME])
+        
+        # get all donuts links
+        donuts_links = self.get_all_donuts_links(item)
+        
+        # add data for given links
+        self.add_data_to_db_for_given_links(donuts_links, manufacturer_id)
+
+        return donuts_links
+    
+
+    def get_all_donuts_links(self, item):
+        '''
+        get links to all donuts for given item (manufacturers web page)
+        and format them
+
+        Args:
+            item: dict(str: str/int) {Scraper.LINK: str (link to a web page),
+                                      Scraper.NAME: str (name of manufacturer),
+                                      Scraper.N: int (Optional, indicates how many subpages to visit)}
+        '''
         donuts_links = []
+
+        # get all links
         if Scraper.N in item.keys():
             donuts_links += self.get_all_donut_pages_with_n_parameter(item)
         else:
             donuts_links += self.get_all_donut_pages_from_given_page(item[Scraper.LINK])
+        
+        # links formatting
+        donuts_links = self.links_formatting(donuts_links, item[Scraper.LINK])
+        
+        print(donuts_links)
+        
+        return donuts_links
+    
+
+    def links_formatting(self, donuts_links, main_link):
+        '''
+        function formatting links to be correct
+
+        Args:
+            donuts_links: List[str]
+            main_link: str (manufacturers web page)
+        '''
         if donuts_links and not donuts_links[0].startswith('https://'):
             add_one = 1 if not donuts_links[0].startswith('/') else 0
-            add = item[Scraper.LINK][0:item[Scraper.LINK].find('/', 8) + add_one]
+            add = main_link[0:main_link.find('/', 8) + add_one]
             donuts_links = [add + link for link in donuts_links]
-        print(donuts_links)
-        self.add_data_to_db_for_given_links(donuts_links, manufacturer_id)
         return donuts_links
 
 
     def get_all_donut_pages_with_n_parameter(self, item):
         '''
-        get all links to subpages with data about donuts 
+        get all links to subpages with data about donuts
+        when there are subpages on manufacturers web page
+
+        Args:
+            item: dict(str: str/int) {Scraper.LINK: str (link to a web page),
+                                      Scraper.NAME: str (name of manufacturer),
+                                      Scraper.N: int (Optional, indicates how many subpages to visit)} 
         '''
         donut_links = []
         for i in range(1, item[Scraper.N]):
@@ -58,8 +112,12 @@ class Scraper:
 
     def get_all_donut_pages_from_given_page(self, html_link):
         '''
-        get all links to subpages with data about donuts 
+        get all links to subpages with data about donuts
+
+        Args:
+            html_link: str (web page html)
         '''
+        # get all links having str('paczek') in it (paczek = donut in Polish)
         with self.get_soup(html_link) as soup:
             return [a['href'] for a in soup.find_all('a') if 'paczek' in a['href']]
 
@@ -68,6 +126,9 @@ class Scraper:
     def get_soup(self, link):
         '''
         context manager returning BeautifulSoup of of given link
+
+        Args:
+            link: str (web page html)
         '''
         r = requests.get(link)
         soup = BeautifulSoup(r.text, features="html.parser")
@@ -81,11 +142,36 @@ class Scraper:
         then add all donuts data to db
 
         Args:
-            links: list[str] - list of links of all donuts subpages
+            links: List[str] - list of links of all donuts subpages
             manufacturer_id: int - id of manufacturer for donuts links list
     
         Returns:
-            None - TODO should be True if added, False if not added
+            bool: True if all donuts were added to bd, False otherwise
+        '''
+        # get all donuts data for adding to db
+        results = self.get_all_donuts_data(links, manufacturer_id)
+        
+        # list of values returned by adding donuts to db
+        added = []
+        
+        for item in results:
+            added.append(
+                self.add_donut(name=item['name'], manufacturer_id=manufacturer_id, kcal=item['kcal'], weight=item['weight'])
+                )
+            
+        return not (len(added) > 0 and None in added)
+    
+
+    def get_all_donuts_data(self, links, manufacturer_id):
+        '''
+        get all data needed to add donuts to db
+
+        Args:
+            links: List[str] - list of links of all donuts subpages
+            manufacturer_id: int - id of manufacturer for donuts links list
+
+        Returns:
+            List[Dict] - Dict contain all data for Donut item
         '''
         results = []
         for link in links:
@@ -93,15 +179,12 @@ class Scraper:
                 name = self.get_name(soup)
                 weight = self.get_weight(soup, name)
                 kcal = self.get_kcal(soup, name)
-                if type(kcal) == type(weight) == type(1):
-                    kcal = kcal * weight / 100
-                print('kcal:', kcal)
                 if name is not None and weight is not None and kcal is not None:
+                    kcal = kcal * weight / 100
                     act_dict = {'name': name, 'kcal': kcal, 'weight': weight, 'manufacturer_id': manufacturer_id}
                     print(act_dict)
                     results.append(act_dict)
-        for item in results:
-            self.add_donut(name=item['name'], manufacturer_id=manufacturer_id, kcal=item['kcal'], weight=item['weight'])
+        return results
 
 
     def get_name(self, soup):
@@ -115,12 +198,9 @@ class Scraper:
             name: Optional(str)
         '''
         names = soup.find_all(class_="active")
-        names = [name for name in names if "pączek" in name.text.lower()]
-        if len(names) == 1:
-            name = names[0].text.strip()
-            print('name:', name)
-            return name
-        names = [x.text for x in soup.select('h1')]
+        names = [name.text for name in names if "pączek" in name.text.lower()]
+        if len(names) != 1:
+            names = [x.text for x in soup.select('h1')]
         if len(names) == 1:
             name = names[0].strip()
             print('name:', name)
@@ -164,6 +244,12 @@ class Scraper:
     def add_manufacturer(self, name):
         '''
         add manufacturer to db
+
+        Args:
+            name: str (name of Manufacturer)
+        
+        Returns:
+            int (manufacturer's id)
         '''
         stmt = select(Manufacturers.id).where(Manufacturers.name == name)
         result = self.db.session.execute(stmt).all()
@@ -181,6 +267,15 @@ class Scraper:
     def add_donut(self, manufacturer_id, name, kcal, weight):
         '''
         add manufacturer to db
+
+        Args:
+            manufacturer_id: int
+            name: str
+            kcal: float
+            weight: int
+        
+        Returns:
+            Optional(List) List if donut added, None otherwise
         '''
         stmt = select(Donuts.id).where(
                                 and_(
@@ -191,7 +286,6 @@ class Scraper:
                                     )
                                 )
         result = self.db.session.execute(stmt).all()
-        print(result)
         if len(result) == 0:
             new_item = Donuts(
                     name=name,
@@ -203,15 +297,19 @@ class Scraper:
             self.db.session.commit()
             print('added', name)
             return self.db.session.execute(stmt).all()
-        return result
+        return None
     
 
     def get_kcal(self, soup, name):
         '''
-        function to find all texts where kcal is in it
-        returns kcal per 100 g if len of all kcal appearances is > 0 and < 4
-        otherwise
-        returns list of all appearances
+        function to find kcal per 100 g
+        
+        Args:
+            soup: BeautifulSoup
+            name: str
+
+        Returns:
+            float: kcals per 100 g
         '''
         kcals = []
         for element in soup.find_all(text=re.compile(r'\d+ kcal')):
